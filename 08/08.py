63756c832bd9acbb0d05080aae85a6c81f5b3485
Tom Minogue <2295063+tminogue@users.noreply.github.com>
2023-12-08 07:10:30 -0500

    day 8, part 1

--- 08/08.py
+++ 08/08.py
@@ -1,1 +1,82 @@
 from common import deserialize_input_file
+
+part1_input_list_test = deserialize_input_file("08a_test_input.txt")
+part1_input_list_puzzle = deserialize_input_file("08a_puzzle_input.txt")
+
+part2_input_list_test = part1_input_list_test
+part2_input_list_puzzle = part1_input_list_puzzle
+
+MOVE_MAP = {"L": 0, "R": 1}
+
+
+def build_node_map(input_list: list[str]) -> dict[str, tuple[str, str]]:
+    node_map = {}
+    node_string_list = input_list[2:]
+    for node_string in node_string_list:
+        node_string_parts = node_string.split(" = ")
+        node_name, node_neighbor_strings = node_string_parts[0], node_string_parts[
+            1
+        ].replace("(", "").replace(")", "").split(", ")
+        node_neighbors = (node_neighbor_strings[0], node_neighbor_strings[1])
+        node_map[node_name] = node_neighbors
+
+    return node_map
+
+
+def run_part_1(input_list: list[str]) -> int:
+    moves = input_list[0]
+    node_map = build_node_map(input_list)
+
+    start = "AAA"
+    destination = None
+
+    hop_count = 0
+    move_index = 0
+    direction = MOVE_MAP[moves[move_index]]
+
+    while destination != "ZZZ":
+        hop_count += 1
+        destination = node_map[start][direction]
+        # print(start, destination)
+        start = destination
+        move_index += 1
+        # reset the index to the start if we reach the end of the moves list
+        if move_index >= len(moves):
+            move_index = 0
+        direction = MOVE_MAP[moves[move_index]]
+
+    return hop_count
+
+
+def run_part_2(input_list: list[str]) -> int:
+    moves = input_list[0]
+    node_map = build_node_map(input_list)
+
+    start = "AAA"
+    destination = None
+
+    hop_count = 0
+    move_index = 0
+    direction = MOVE_MAP[moves[move_index]]
+
+    while destination != "ZZZ":
+        hop_count += 1
+        destination = node_map[start][direction]
+        # print(start, destination)
+        start = destination
+        move_index += 1
+        # reset the index to the start if we reach the end of the moves list
+        if move_index >= len(moves):
+            move_index = 0
+        direction = MOVE_MAP[moves[move_index]]
+
+    return hop_count
+
+
+print("Part 1:")
+print(f"  Test input yields: {run_part_1(part1_input_list_test)}")
+print(f"  Puzzle input yields: {run_part_1(part1_input_list_puzzle)}")
+
+print("\nPart 2:")
+print(f"  Test input yields: {run_part_2(part2_input_list_test)}")
+print(f"  Puzzle input yields: {run_part_2(part2_input_list_puzzle)}")
